61e76c1b37269ba55a03eccb28758ce160af11f4
Myroslav Tovtyk <tovmyr@gmail.com>
2022-08-14 14:59:23 +0300

    fixed

--- src/js/delete-button-event.js
+++ src/js/delete-button-event.js
@@ -5,11 +5,12 @@ import keys from './local-storage-keys';
 
 const onClickDeleteBtn = event => {
   const contactID = event.target.dataset.id;
-  const newData = data.filter(item => item.id !== contactID);
-  if (newData.length === data.length) return;
-  data.splice(0, data.length, ...newData);
-  refs.list.innerHTML = data.map(contact => createMarkup(contact)).join('');
-  localStorage.setItem(keys.CONTACTS, JSON.stringify(newData));
+  const newData = JSON.parse(localStorage.getItem(keys.CONTACTS)) ?? data
+  const filteredData = newData.filter(item => item.id !== contactID);  
+  if (filteredData.length === newData.length) return;
+  newData.splice(0, newData.length, ...filteredData);
+  refs.list.innerHTML = newData.map(contact => createMarkup(contact)).join('');
+  localStorage.setItem(keys.CONTACTS, JSON.stringify(filteredData));
 };
 
 refs.list.addEventListener('click', onClickDeleteBtn);
