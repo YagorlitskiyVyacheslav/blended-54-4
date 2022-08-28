82085f091d0e8fad8a04a57b361aa56adcf9d28d
Oleksii Kravchenko <djdesidos@gmail.com>
2022-08-28 16:35:04 +0300

    add

--- src/weather-js/refs.js
+++ src/weather-js/refs.js
@@ -3,4 +3,6 @@ export const refs = {
   searchForm: document.getElementById('search-form'),
   weatherContainer: document.getElementById('current_weather_container'),
   fiveDaysContainer: document.getElementById('five_days_container'),
+  fiveDaysBtn: document.getElementById('five_day_btn'),
+  todayBtn: document.getElementById('today_btn'),
 };
